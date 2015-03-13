0bb72d6bdf1e813710714bdfa31935753e118d2e
Animaxx <Animax.Deng@gmail.com>
2015-03-13 12:00:59 -0500

    rebuilt

--- Product/A_IOSHelper.framework/Headers/UIView+A_Extension.h
+++ Product/A_IOSHelper.framework/Headers/UIView+A_Extension.h
@@ -11,11 +11,26 @@
 @interface UIView (A_Extension)
 
 - (void) A_Animation_FadeIn: (double)duration;
+- (void) A_Animation_FadeIn: (double)duration WhenCompleted:(void (^)(void))block;
 - (void) A_Animation_FadeOut: (double)duration;
+- (void) A_Animation_FadeOut: (double)duration WhenCompleted:(void (^)(void))block;
 
 - (void) A_Animation_MoveOut_FromLeft: (double)duration;
+- (void) A_Animation_MoveOut_FromLeft: (double)duration WhenCompleted:(void (^)(void))block;
 - (void) A_Animation_MoveOut_FromRight: (double)duration;
+- (void) A_Animation_MoveOut_FromRight: (double)duration WhenCompleted:(void (^)(void))block;
 - (void) A_Animation_MoveOut_FromTop: (double)duration;
+- (void) A_Animation_MoveOut_FromTop: (double)duration WhenCompleted:(void (^)(void))block;
 - (void) A_Animation_MoveOut_FromBottom: (double)duration;
+- (void) A_Animation_MoveOut_FromBottom: (double)duration WhenCompleted:(void (^)(void))block;
+
+- (void) A_Animation_MoveToCenter: (double)duration;
+- (void) A_Animation_MoveToCenter: (double)duration WhenCompleted:(void (^)(void))block;
+
+- (void) A_Animation_ToSize: (double)duration Size: (CGSize)size;
+- (void) A_Animation_ToSize: (double)duration Size: (CGSize)size WhenCompleted:(void (^)(void))block;
+
+- (void) A_Animation_ToFullScreenSize: (double)duration;
+- (void) A_Animation_ToFullScreenSize: (double)duration WhenCompleted:(void (^)(void))block;
 
 @end
